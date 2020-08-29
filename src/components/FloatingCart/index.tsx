89b824cb1cc211e1c0e7be58b3c4a083f55b817c
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-08-29 17:41:06 -0300

    Finalizando o desafio

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,23 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulator, prodct) => {
+      const productsSubTotal = prodct.price * prodct.quantity;
 
-    return formatValue(0);
+      return accumulator + productsSubTotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, prodct) => {
+      const productsAmount = prodct.quantity;
 
-    return 0;
+      return accumulator + productsAmount;
+    }, 0);
+
+    return total;
   }, [products]);
 
   return (
